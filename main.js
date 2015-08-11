2acfb015670d75037d05378823350ecbe7ee879f
Marcelo Hudson <marcelo.hudson54@gmail.com>
2015-08-11 21:45:24 +1200

    req change

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ app.post('/', function(req, res){
 	var searchTerms = req.query.text;
 	var channel = req.query.channel;
 
-	console.log('token: '+ req.body.token);
+	console.log('token: '+ req.token);
 
 	//check Slash Command token
 	if (req.query.token !== slashCommandToken) {
